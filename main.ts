64697a4a7b533ca8b4146a46032be836bdf290c5
attudevalck <maximknockaert@gmail.com>
2025-02-27 14:13:53 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,4 @@
+Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, 50)
+Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CCW, 50)
+basic.pause(532)
+Maqueen_V5.motorStop(Maqueen_V5.Motors.All)
